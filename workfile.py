d453f9e824c2c27299ba1d136c210472fa2af7d0
Nipun Sood <nipunsood97@gmail.com>
2021-09-19 17:01:35 +0530

    Create workfile.py

---
+++ workfile.py
@@ -0,0 +1,22 @@
+def simple_print():
+    '''
+    Print "Hello world" here using a simple print statement
+    '''
+    # TODO
+    
+def end_print():
+    '''
+    Print all the items from the given list here using print statements in a loop, separated by underscores "_", using the 'end' argument
+    '''
+    fruits = ['apple', 'banana', 'guava', 'watermelon']
+    for f in fruits:
+        # TODO
+    # Bonus: Modify above code as needed (except the fruits list) to print the result in such a way that there isn't an extra "_" at the end.
+    # Do you need an extra print statement here?
+    
+def separator_print():
+    '''
+    Print all the items from the given list here using a single print statement, separated by underscores "_", using the 'separator' argument
+    '''
+    fruits = ['apple', 'banana', 'guava', 'watermelon']
+    # TODO
